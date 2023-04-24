d065bf1eaeb19e63c2691f2193fb5ca732c60875
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2023-04-23 18:29:08 -0700

    Made changes to navbar and index.js to adjust for login, and fixed import error in Account related routes

--- quizfeed-frontend/src/components/GlobalNavbar.js
+++ quizfeed-frontend/src/components/GlobalNavbar.js
@@ -2,7 +2,7 @@ import './GlobalNavbar.css';
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 // FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
@@ -10,39 +10,61 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 export default function GlobalNavbar() {
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const unAuthPaths = ['/', '/register', '/forgotpassword'];
 
     const handleLogout = async () => {
       try {
         const response = await fetch('/logout', {
           method: 'GET',
         });
-        navigate('/login');
+        navigate('/');
         } catch (err) {
             console.error(err);
         }
     }
 
+    const checkPath = unAuthPaths.includes(location.pathname);
+
     return (
-        <Navbar className='globalNav' bg='dark' variant='dark' expand='lg' sticky='top'>
-            <Nav.Link as={NavLink} to='/' className='ms-5 me-3'>
-                <Navbar.Brand className='brand'>
+        <Navbar className="globalNav" bg="dark" variant="dark" expand="lg" sticky="top">
+            <Nav.Link as={NavLink} to="/" className="ms-5 me-3">
+                <Navbar.Brand className="brand">
                     Q<br />F
                 </Navbar.Brand>
             </Nav.Link>
-            <Navbar.Toggle aria-controls='navbar-nav' />
-            <Navbar.Collapse id='navbar-nav'>
-                <Nav className='me-auto'>
-                    <Nav.Link as={NavLink} to='/home' className='mx-3'>HOME <span className='visually-hidden'>(current)</span></Nav.Link>
-                    <Nav.Link as={NavLink} to='/' className='mx-3'>TRENDING QUIZZES</Nav.Link>
-                    <Nav.Link as={NavLink} to='/' className='mx-3'>NEW QUIZZES</Nav.Link>
-                    <Nav.Link as={NavLink} to='/' className='mx-3'>SEARCH</Nav.Link>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link as={NavLink} to="/home" className="mx-3" disabled={checkPath}>
+                        HOME <span className="visually-hidden">(current)</span>
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="#" className="mx-3" disabled={checkPath}>
+                        TRENDING QUIZZES
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="#" className="mx-3" disabled={checkPath}>
+                        NEW QUIZZES
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="#" className="mx-3" disabled={checkPath}>
+                        SEARCH
+                    </Nav.Link>
                 </Nav>
-                <Nav className='ms-auto me-5'>
-                    <Nav.Link as={NavLink} to='/friends' className='mx-3'>FRIENDS</Nav.Link>
-                    <Nav.Link as={NavLink} to='/myAccount'>
+                <Nav className="ms-auto me-5">
+                    <Nav.Link as={NavLink} to="/friends" className="mx-3" disabled={checkPath}>
+                        FRIENDS
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="/myAccount" disabled={checkPath}>
                         <FontAwesomeIcon icon={faUser} />
                     </Nav.Link>
-                    <Nav.Link as={NavLink} to='/logout' className='mx-3' onClick={handleLogout}>LOGOUT</Nav.Link>
+                    {checkPath ? (
+                        <Nav.Link as={NavLink} to="/" className="mx-3">
+                            LOGIN
+                        </Nav.Link>
+                    ) : (
+                        <Nav.Link as={NavLink} to="/logout" className="mx-3" onClick={handleLogout}>
+                            LOGOUT
+                        </Nav.Link>
+                    )}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
